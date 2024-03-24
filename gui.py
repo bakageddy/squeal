7d11e796e3387172e6beec9d8ae1b2331d53f626
Dinesh Kumar G <gdineshda@gmail.com>
2024-03-24 18:54:11 +0530

    Add Input Widget Class

--- gui.py
+++ gui.py
@@ -16,23 +16,10 @@ class Input:
         self.input_field.grid(row=row, column=(col + 1))
 
     def get_input(self):
-        self.input_field.get()
+        return self.input_field.get()
 
 
 def init(title: str) -> tk.Tk:
     window = tk.Tk()
     window.title(title)
     return window
-
-
-window = init("My Tkinter GUI")
-frame = tk.Frame(window)
-frame.grid()
-
-book_id = Input(frame, "Book ID:")
-book_id.create()
-book_id.set_grid_loc(0, 0)
-
-author = Input(frame, "Author: ")
-author.create()
-author.set_grid_loc(1, 0)
